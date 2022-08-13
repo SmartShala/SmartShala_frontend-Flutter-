1c9cb2e893ccacee6571ca6f0666c31a2dd6931c
Sayak Bhattacharjee <sayak718.s@gmail.com>
2022-08-14 04:08:13 +0530

    scaner option added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_shala/Views/camera.dart';
 import 'package:smart_shala/Views/dash.dart';
 import 'package:smart_shala/Views/home.dart';
 import 'Views/login.dart';
 import 'Views/register.dart';
 import 'Views/main_view.dart';
 
-void main() {
+Future<CameraDescription> cam() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+  return firstCamera;
+}
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+
+  // // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -27,7 +44,8 @@ void main() {
       '/register/': (context) => const RegisterView(),
       '/mainpage/': (context) => const MainPage(),
       '/dashboard/': (context) => const Dashboard(title: "Dashboard"),
-      '/home': (context) => const Homepage_view()
+      '/hom/': (context) => const Homepage_view(),
+      '/camera/': (context) => TakePictureScreen(camera: firstCamera)
     },
   ));
 }
